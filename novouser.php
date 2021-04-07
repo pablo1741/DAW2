b38958f04af6fa760d7c7f538cbd39e3ef83d788
pablo1741 <62766663+pablo1741@users.noreply.github.com>
2021-04-06 22:22:05 -0300

    Create novouser.php

---
+++ novouser.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'conectabd.php';
+include_once 'funcDAO.php';
+include_once 'funcionario.php';
+
+$funcionario= new funcionario ();
+$funcionario->nome=$_POST["nome"];
+$funcionario->email=$_POST["email"];
+$funcionario->senha=$_POST["senha"];
+$funcionario->nfunc=$_POST["nfunc"];
+$funcionario->celular=$_POST["celular"];
+$funcionario->adm=$_POST["adm"];
+$resultado=$funcionario->inserir();
+/*
+echo "$funcionario"."<br>";
+if (!isset($funcionario)) {
+    echo "$funcionario";
+$fdao= new funcDao();
+$fdao->inserir($funcionario);
+
+echo "//////////////////////<br>";
+$a=$fdao->retornar();
+var_dump($a);
+foreach ($a as $value) {
+echo $value['nome'].",";
+echo $value['email'].",";
+echo $value['senha'].",";
+echo $value['nfunc'].",";
+echo $value['celular'].",";
+echo $value['adm']."<br>";
+}
+}*/
